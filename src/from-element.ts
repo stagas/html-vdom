447d0f7d411563fb985b24fe459835f60e33382e
stagas <gstagas@gmail.com>
2022-07-30 03:41:59 +0300

    feature: add custom element getName polyfill

--- src/from-element.ts
+++ src/from-element.ts
@@ -1,32 +1,74 @@
+import type { Class } from 'everyday-types'
+
+import { kebab } from 'everyday-utils'
 import * as jsxi from 'html-jsx'
+
 import { jsx } from './jsx-runtime'
 
-export type Constructor<T> = new(...args: any[]) => T
+const components: WeakMap<typeof HTMLElement, any> = new WeakMap()
 
-const Registry: WeakMap<typeof HTMLElement, any> = new WeakMap()
-
-export interface Options<T> extends ElementDefinitionOptions {
-  interface?: Constructor<T>
+// until a getName() is implemented or similar
+// we polyfill it to capture the tagNames
+// of foreign custom elements
+// https://github.com/WICG/webcomponents/issues/566
+declare const customElements: CustomElementRegistry & {
+  getName(ctor: CustomElementConstructor): string | undefined
+}
+if (!customElements.getName) {
+  const tags: WeakMap<typeof HTMLElement, string> = new WeakMap()
+  const { define } = customElements
+  customElements.define = function(
+    name: string,
+    ctor: CustomElementConstructor,
+    options?: ElementDefinitionOptions,
+  ): void {
+    tags.set(ctor, name)
+    return define.call(customElements, name, ctor, options)
+  }
+  customElements.getName = function(ctor: CustomElementConstructor) {
+    return tags.get(ctor)
+  }
 }
 
-export type Component<T, I> = (props: Partial<Omit<T & I, keyof HTMLElement>> & jsxi.HTMLAttributes<T>) => any
+export interface Options<T> extends ElementDefinitionOptions {
+  interface?: Class<T>
+}
+
+export type Component<T, I> =
+  & {
+    /** Returns the tag name of the component. */
+    toString(): string
+  }
+  & ((props: Partial<Omit<T & I, keyof HTMLElement>> & jsxi.HTMLAttributes<T>) => JSX.Element)
+
+let suffixKey = 0
 
 export const fromElement = <T extends HTMLElement, I = HTMLElement>(
-  Element: Constructor<T>,
-  options: Options<I> = {},
+  ctor: Class<T> & { elementOptions?: Options<I> },
+  options: Options<I> = ctor.elementOptions ?? {},
 ): Component<T, I> => {
-  let fn: Component<T, I> = Registry.get(Element)
+  let fn: Component<T, I> = components.get(ctor)
   if (fn) return fn
 
-  const tag = 'x-' + Element.name.toLowerCase() + ((Math.random() * 1e7) | 0).toString(36)
-  const ctor = class extends Element {}
-  customElements.define(tag, ctor, options)
+  let tag = customElements.getName(ctor)
+
+  if (!tag) {
+    tag = kebab(ctor.name).replace('-element', '')
+    if (!tag.includes('-')) tag = 'x-' + tag
+    while (customElements.get(tag) != null) {
+      tag += (++suffixKey).toString(36)
+    }
+    customElements.define(tag, ctor, options)
+  }
+
   if (options.extends)
     fn = props => jsx(options.extends, Object.assign(props, { is: tag }), void 0)
   else
     fn = props => jsx(tag, props, void 0)
-  Registry.set(Element, fn)
-  ;(fn as any).toString = () => tag
+
+  components.set(ctor, fn)
+
+  fn.toString = () => tag!
 
   return fn
 }
